4ced65bda620790d15c84690ec6f45411d85c239
Jeremiah Haney <nemesis4343@hotmail.com>
2026-09-05 11:25:10 -0700

    Use repository antiforgery endpoint extension

--- src/ClarityBelongs.Web/Services/AntiforgeryEndpointExtensions.cs
+++ src/ClarityBelongs.Web/Services/AntiforgeryEndpointExtensions.cs
@@ -1,28 +1,5 @@
-using Microsoft.AspNetCore.Antiforgery;
-
 namespace ClarityBelongs.Web.Services;
 
-public static class AntiforgeryEndpointExtensions
+internal static class AntiforgeryEndpointCompatibility
 {
-    public static RouteHandlerBuilder RequireAntiforgery(
-        this RouteHandlerBuilder builder)
-    {
-        return builder.AddEndpointFilter(
-            async (context, next) =>
-            {
-                var antiforgery = context.HttpContext.RequestServices
-                    .GetRequiredService<IAntiforgery>();
-
-                try
-                {
-                    await antiforgery.ValidateRequestAsync(context.HttpContext);
-                }
-                catch (AntiforgeryValidationException)
-                {
-                    return Results.BadRequest();
-                }
-
-                return await next(context);
-            });
-    }
 }
