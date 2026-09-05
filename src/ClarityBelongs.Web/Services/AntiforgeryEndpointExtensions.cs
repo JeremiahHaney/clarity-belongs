6a6ba6907c387e6a6537e644f244b0113a8cf42c
Jeremiah Haney <nemesis4343@hotmail.com>
2026-09-05 11:23:33 -0700

    Enforce standard antiforgery on minimal API form endpoints

---
+++ src/ClarityBelongs.Web/Services/AntiforgeryEndpointExtensions.cs
@@ -0,0 +1,28 @@
+using Microsoft.AspNetCore.Antiforgery;
+
+namespace ClarityBelongs.Web.Services;
+
+public static class AntiforgeryEndpointExtensions
+{
+    public static RouteHandlerBuilder RequireAntiforgery(
+        this RouteHandlerBuilder builder)
+    {
+        return builder.AddEndpointFilter(
+            async (context, next) =>
+            {
+                var antiforgery = context.HttpContext.RequestServices
+                    .GetRequiredService<IAntiforgery>();
+
+                try
+                {
+                    await antiforgery.ValidateRequestAsync(context.HttpContext);
+                }
+                catch (AntiforgeryValidationException)
+                {
+                    return Results.BadRequest();
+                }
+
+                return await next(context);
+            });
+    }
+}
